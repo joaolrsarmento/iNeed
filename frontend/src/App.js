faf266eb2abab781765baa18bcfda4fc7d2772c0
João Sarmento <joaolrsarmento@gmail.com>
2020-03-29 18:05:22 -0300

    Routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.png';
-import './App.css';
+
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Routes />
   );
 }
 
