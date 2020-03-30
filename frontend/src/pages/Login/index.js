81bfbdf37c8f2a7f8787bed7ec05c47d7afa5f47
João Sarmento <joaolrsarmento@gmail.com>
2020-03-29 23:28:54 -0300

    Login and registration pages created

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import Header from '../../components/Header';
+import {Link} from 'react-router-dom';
 
-export default function Login(){
+import instaLogo from '../../assets/instalogo.png';
+import faceLogo from '../../assets/facelogo.png';
+import './styles.css';
+
+
+export default function Login() {
+
     return (
-        <div className="login-container">
-            <Header />
+        <div>
+            <div className="container" id="container">
+                <div className="form-container sign-in-container">
+                    <form action="/">
+                        <h1>Sign in</h1>
+                        <div className="social-container">
+                            <a href="/facebook" className="social"><i className="fab fa-facebook-f"><img src={faceLogo} alt="face"class="media-logo"/></i></a>
+                            <a href="/googleplus" className="social"><i className="fab fa-google-plus-g"><img src={instaLogo} alt="insta" class="media-logo"/></i></a>
+                        </div>
+                        <span>or use your account</span>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <a href="/recovery">Forgot your password?</a>
+                        <button>Sign In</button>
+                    </form>
+                </div>
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details and start journey with us</p>
+                            <Link to = "/register"><button class="ghost" id="signUn">Sign Up</button></Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 
 }
+
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import instaLogo from '../../assets/instalogo.png';
+import faceLogo from '../../assets/facelogo.png';
+
+import './styles.css';
+
+
+export default function Register() {
+
+    return (
+        <div>
+            <div class="container" id="container">
+
+                <div class="form-container sign-up-container">
+                    <form action="/">
+                        <h1>Create Account</h1>
+                        <div class="social-container">
+                            <a href="/facebook" class="social"><i class="fab fa-facebook-f"><img src={faceLogo} alt = "face" class="media-logo"/></i></a>
+                            <a href="/googleplus" class="social"><i class="fab fa-google-plus-g"><img src={instaLogo} alt ="insta" class="media-logo"/></i></a>
+                        </div>
+                        <span>or use your email for registration</span>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button>Sign Up</button>
+                    </form>
+                </div>
+                <div class="overlay-container">
+                    <div class="overlay">
+                        <div class="overlay-panel overlay-right">
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <Link to = "/"><button class="ghost" id="signIn">Sign In</button></Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+
+}
+
