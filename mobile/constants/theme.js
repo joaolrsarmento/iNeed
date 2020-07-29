c75eeece6b402a67a0393f99c4c6c1696fb81c75
João Sarmento <joaolrsarmento@gmail.com>
2020-07-28 22:10:15 -0300

    added colors

--- mobile/constants/theme.js
+++ mobile/constants/theme.js
@@ -1,12 +1,12 @@
 const colors = {
   accent: "#F3534A",
-  primary: "#0AC4BA",
-  secondary: "#2BDA8E",
+  primary: "#FF4B2B",
+  secondary: "#FFA500",
   tertiary: "#FFE358",
   black: "#323643",
   white: "#FFFFFF",
   gray: "#9DA3B4",
-  gray2: "#C5CCD6"
+  gray2: "#C5CCD6",
 };
 
 const sizes = {
